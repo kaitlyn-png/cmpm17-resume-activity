6badf32b99dc11ac38986d6c665ff3e8811ff2a0
Kaitlyn Eng <123908591+kaitlyn-png@users.noreply.github.com>
2026-01-13 18:54:42 -0800

    implemented current age w/ typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,23 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'cmpm17-resume-activity';
+  title = 'Kaitlyn Eng Resume';
+
+  birthDate: Date = new Date(2004, 8, 10);
+  
+  currentDate: Date = new Date();
+  currentMonth: number = this.currentDate.getMonth() + 1;
+  currentYear: number =this.currentDate.getFullYear();
+  currentDay: number = this.currentDate.getDate();
+
+  getAge(): number {
+    let age = this.currentYear - this.birthDate.getFullYear();
+    if (
+      this.currentMonth < this.birthDate.getMonth() + 1 ||
+      (this.currentMonth === this.birthDate.getMonth() + 1 && this.currentDay < this.birthDate.getDate())
+    ) {
+      age--;
+    }
+    return age;
+  }
 }
