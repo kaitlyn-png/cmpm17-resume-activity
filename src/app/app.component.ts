217067743151d065247cfa3a4b1e76fe3031ab83
Kaitlyn Eng <123908591+kaitlyn-png@users.noreply.github.com>
2026-02-17 18:54:27 -0800

    added relevant course work section --> need to move into a component + added projects and experience component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { Experience } from "./experience/experience.model";
+import { EXPERIENCES } from "./experience/experience.mock";
+import { Signal } from '@angular/core';
+import { computed } from '@angular/core';
 
 @Component({
   selector: 'app-component',
-  imports: [
-    MatExpansionModule,
-  ],
+  imports: [ MatExpansionModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Kaitlyn Eng Resume';
-
   birthDate: Date = new Date(2004, 8, 10);
   
   currentDate: Date = new Date();
@@ -30,4 +30,40 @@ export class AppComponent {
     }
     return age;
   }
+
+  name: string = 'Kaitlyn Eng';
+  title = 'Kaitlyn Eng Resume';
+  profilePic: string = 'images/kaitlyn-image.jpg';
+  leadership: string = '';
+  skills: string[] = [];
+  isAdmin = true;
+
+  // ------------------------- INPUTS AND OUTPUTS -------------------------
+
+  // ------------------------- LOCAL UI STATE -----------------------------
+
+  /** Working Experience **/
+  experiences : Experience[] = EXPERIENCES;
+
+  // ------------------------- WRITABLE SIGNALS ---------------------------
+  selectedName: WritableSignal<string>= signal('Jialai');
+
+  // ------------------------- COMPUTED DATA ------------------------------
+
+  /** Computed signal that store my working experience **/
+  myExperience: Signal<Experience | undefined > = computed( () => {
+    return this.experiences.find(
+      exp => exp.name === this.selectedName()
+    )
+  })
+
+  // ------------------------- EVENT HANDLING -----------------------------
+
+  changeStudent(){
+    this.selectedName.set('Kaitlyn');
+  }
+
+  // ------------------------- OTHER --------------------------------------
+
+  // ------------------------- LOAD AND CLEANUP ---------------------------
 }
